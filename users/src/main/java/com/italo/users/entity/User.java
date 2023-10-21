54c7ee66cfed55213942083da0f1a56ccc5ce03d
italo <italomauricio98@gmail.com>
2023-10-21 13:04:49 -0300

    create class user

---
+++ users/src/main/java/com/italo/users/entity/User.java
@@ -0,0 +1,26 @@
+package com.italo.users.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+    @Column(name = "age", nullable = false)
+    private Integer age;
+}
