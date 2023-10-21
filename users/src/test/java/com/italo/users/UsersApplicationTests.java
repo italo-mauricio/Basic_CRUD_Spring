4728a0079666095005c708f19c1d0d244a217b04
italo <italomauricio98@gmail.com>
2023-10-21 12:51:22 -0300

    first commit in repository

---
+++ users/src/test/java/com/italo/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
